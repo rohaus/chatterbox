37880929d2c2169744f8d8f5de7d4882409e08a9
Hack Reactor Students <github@hackreactor.com>
2013-10-07 20:54:25 -0700

    Secures from XSS, adds username and room name inputs.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,28 +1,39 @@
 var url = 'https://api.parse.com/1/classes/chatterbox';
-//display()
+var rooms = {};
+var secure = function(key){
+  if (key === undefined || key === null){
+    return undefined;
+  }
+  return key.replace("<", "&lt;").replace(">","&gt;");
+}
+
 var display = function(messages){
-  var list, results = messages.results;
+  var list, option, results = messages.results;
   $('.chatbox').text('');
-  console.log('clearing chat box');
-  for(var i = results.length - 10; i < results.length; i++){
+  $('select').text('');
+  for(var i = results.length-1 ; i >= results.length - 10; i--){
     var message = results[i];
-    if (message.username === undefined){
-      message.username = "anonymous";
-    }
-    list = '<li class="message">'+message.username+": "+message.text+'</li>';
+    message.username = secure(message.username) || username;
+    message.roomname = secure(message.roomname) || 'lobby';
+    message.text = secure(message.text) || '';
+    rooms[message.roomname] = message.roomname;
+    list = '<li class="message">('+message.roomname+') '+message.username+": "+message.text+'</li>';
     $('.chatbox').append(list);
+  }
+  for(var room in rooms){
+    var option = '<option>'+room+'</option>';
+    $('select').append(option);
   }
 }
 
-
-//fetch()
 var fetch = function(callBack){
   return $.ajax({
     url: url,
     type: 'GET',
     contentType: 'application/json',
     data : {
-      limit: 10
+      limit: 10,
+      order: "-createdAt"
     },
     success: function(messages){
       console.log("Messages fetched");
@@ -31,19 +42,14 @@ var fetch = function(callBack){
     error: function(){
       console.log('Error fetching messages');
     }
-    });
+  });
 };
-fetch(display);
-setInterval(function(){
-  fetch(display);
-}, 5000);
 
-//send()
 var send = function(msg){
   var message = {
-    'username': 'ken2',
+    'username': username,
     'text': msg,
-    'roomname': '4chan'
+    'roomname': $('select').val() || 'lobby'
   };
   $.ajax({
   	url: url,
@@ -57,5 +63,19 @@ var send = function(msg){
   		console.log('Error sending message');
   	}
   });
+  fetch(display);
 };
-send("abcefh");
+
+// create rooms for users to chat in
+/*
+  On click
+*/
+
+// friends
+/*
+filter by friends using bold text
+*/
+
+var username = prompt("What is your username?");
+fetch(display);
+setInterval(function(){fetch(display);}, 5000);
