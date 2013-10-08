a01ac656856549538137639bfa408d27d0937ed7
Hack Reactor Students <github@hackreactor.com>
2013-10-08 12:08:41 -0700

    Displays messages by room.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,48 +1,69 @@
 var url = 'https://api.parse.com/1/classes/chatterbox';
+var timer;
 var rooms = {};
 var secure = function(key){
   if (key === undefined || key === null){
     return undefined;
   }
-  return key.replace("<", "&lt;").replace(">","&gt;");
+  return key.replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("&", "&amp;")
+            .replace('"', "&quot;")
+            .replace("'", "&#x27;")
+            .replace("/", "&#x2F;");
 }
 
-var display = function(messages){
+var filterMessages = function(className){
+  // debugger;
+  $('.message').css({'display':'none'});
+  $('.message').filter('.'+className).css({'display':'block'});
+};
+
+var updateRooms = function(){
+  $('select').text('');
+  for(var room in rooms){
+    var option = '<option>'+room+'</option>';
+    $('select').append(option);
+  }
+};
+
+var display = function(messages, filter){
   var list, option, results = messages.results;
   $('.chatbox').text('');
-  $('select').text('');
-  for(var i = results.length-1 ; i >= results.length - 10; i--){
+  // debugger;
+  for(var i = results.length-1 ; i >= results.length - 50; i--){
     var message = results[i];
     message.username = secure(message.username) || username;
     message.roomname = secure(message.roomname) || 'lobby';
     message.text = secure(message.text) || '';
     rooms[message.roomname] = message.roomname;
-    list = '<li class="message">('+message.roomname+') '+message.username+": "+message.text+'</li>';
+    list = '<li class="message ' + message.roomname +'">('+message.roomname+') '+message.username+": "+message.text+'</li>';
     $('.chatbox').append(list);
   }
-  for(var room in rooms){
-    var option = '<option>'+room+'</option>';
-    $('select').append(option);
-  }
-}
+  updateRooms();
+  filterMessages(filter);
+};
 
-var fetch = function(callBack){
-  return $.ajax({
+var fetch = function(filter){
+  $.ajax({
     url: url,
     type: 'GET',
     contentType: 'application/json',
     data : {
-      limit: 10,
-      order: "-createdAt"
+      limit: 50,
+      order: "-createdAt",
     },
     success: function(messages){
       console.log("Messages fetched");
-      callBack(messages);
+      display(messages, filter);
     },
     error: function(){
       console.log('Error fetching messages');
     }
   });
+  // debugger;
+  clearTimeout(timer);
+  timer = setInterval(function(){fetch(filter);}, 5000);
 };
 
 var send = function(msg){
@@ -63,8 +84,9 @@ var send = function(msg){
   		console.log('Error sending message');
   	}
   });
-  fetch(display);
 };
+
+var username = prompt("What is your username?");
 
 // create rooms for users to chat in
 /*
@@ -76,6 +98,7 @@ var send = function(msg){
 filter by friends using bold text
 */
 
-var username = prompt("What is your username?");
-fetch(display);
-setInterval(function(){fetch(display);}, 5000);
+// var filter = null;
+// fetch(display);
+// // updateRooms();
+fetch();
